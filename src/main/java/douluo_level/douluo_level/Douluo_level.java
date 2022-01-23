ec87b871e8cc2f6c25f03f3351c8c4ae7c1e195f
EnderTheCoder <1991455223@qq.com>
2022-01-23 16:24:41 +0800

    Initial commit

---
+++ src/main/java/douluo_level/douluo_level/Douluo_level.java
@@ -0,0 +1,17 @@
+package douluo_level.douluo_level;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Douluo_level extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
